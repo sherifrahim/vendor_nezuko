502827a6d5b6b282f2227a562ae581f4ebc114d8
sairpa <pranav.ajay2000@gmail.com>
2021-06-22 17:35:56 +0530

    Nezuko: Ship 720P bootanimation for cringe 720P devices

--- config/bootanimation.mk
+++ config/bootanimation.mk
@@ -13,5 +13,18 @@
 # limitations under the License.
 
 #Bootanimation
+LOCAL_PATH := vendor/nezuko
 
-PRODUCT_COPY_FILES += vendor/nezuko/prebuilt/common/bootanimation/bootanimation.zip:system/media/bootanimation.zip
+ifeq ($(TARGET_BOOT_ANIMATION_RES),720)
+# Why do these exist in 2021 ewwwwwwwwww
+     PRODUCT_COPY_FILES += $(LOCAL_PATH)/prebuilt/common/bootanimation/720.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
+else ifeq ($(TARGET_BOOT_ANIMATION_RES),1080)
+     PRODUCT_COPY_FILES += $(LOCAL_PATH)/prebuilt/common/bootanimation/bootanimation.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
+else
+    ifeq ($(TARGET_BOOT_ANIMATION_RES),)
+        $(warning "TARGET_BOOT_ANIMATION_RES is undefined, assuming 1080p")
+    else
+        $(warning "Current bootanimation res is not supported, forcing 1080p")
+    endif
+    PRODUCT_COPY_FILES += $(LOCAL_PATH)/prebuilt/common/bootanimation/bootanimation.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
+endif
